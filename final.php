36df725c2028659a9cb9f56cf94a59f44f61096a
JohnQuimson <john.quimson777@gmail.com>
2024-01-08 19:43:49 +0100

    feat: style

--- final.php
+++ final.php
@@ -13,32 +13,44 @@ $replace = str_replace($badword, "***", $testo);
   <meta charset="UTF-8">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
   <title>PHP Badwords</title>
+
+   <!-- Boostrap CSS -->
+  <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous">
+  <!-- Boostrap CSS -->
+  
   <!-- My CSS -->
   <link rel="stylesheet" href="css/style.css">
 </head>
 <body>
-  <h1>PHP Badwords</h1>
+  <h1>PHP Bad-words</h1>
 
   <main>
 
+  <div class="container">
+
     <!-- Raw text -->
-    <strong>Raw Text</strong>
-    <p>
+    <div class="cont-raw">
+      <strong>Raw Text</strong>
+    <p class="testo">
       <?php 
         echo $testo;
       ?>
     </p>
-    <span>Sono presenti:  <?php echo strlen($testo); ?> caratteri.</span>
+    <p>Sono presenti:  <span><?php echo strlen($testo); ?></span> caratteri.</p>
+    </div>
 
     <!-- Modified -->
-    <strong>Modified</strong>
-    <p>
+   <div class="cont-modified">
+     <strong>Modified</strong>
+    <p class="testo">
       <?php 
         echo $replace;
       ?>
     </p>
-     <span>Sono presenti:  <?php echo strlen($replace); ?> caratteri.</span>
+         <p>Sono presenti:  <span><?php echo strlen($replace); ?></span> caratteri.</p>
 
+   </div>
+</div>
   </main>
 
 </body>
--- index.php
+++ index.php
@@ -4,29 +4,32 @@
   <meta charset="UTF-8">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
   <title>PHP Badwords</title>
+
+  <!-- Boostrap CSS -->
+  <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous">
+  <!-- Boostrap CSS -->
+
   <!-- My CSS -->
   <link rel="stylesheet" href="css/style.css">
 </head>
 <body>
-  <h1>PHP Badwords</h1>
+  <h1>PHP Bad-words</h1>
 
 
-  <form action="final.php" method="post">
+  <div class="container">
+    <form class="row" action="final.php" method="post">
 
-    <!-- Campo di testo -->
-    <label for="testo">Testo:</label>
-   <textarea name="testo" id="testo" cols="30" rows="2" placeholder="Inserisci il testo"></textarea>
-    
-   <!-- Bad Words -->
-   <label for="badword">Bad-words:</label>
-    <input type="text" id="badword" name="badword" placeholder="Inserisci le bad-words">
-    <button>invia</button>
-  </form>
+       <!-- Campo di testo -->
+       <label for="testo" class="form-label">Testo:</label>
+      <textarea name="testo" class="form-control" id="testo" cols="30" rows="3"></textarea> 
 
-
-  
-    
-  
-
+       <!-- Bad Words -->
+      <label for="badword" class="mt-5">Bad-words:</label>
+      <input type="text" id="badword" name="badword" placeholder="Inserisci le bad-words">
+      <div class="col-12 p-0 mt-3">
+        <button type="submit" class="btn btn-primary">Invia</button>
+      </div>
+    </form>
+  </div>
 </body>
 </html>
