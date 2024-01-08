eb5d0f21ec18731ccea3210bfeaeb8488c49ca1a
JohnQuimson <john.quimson777@gmail.com>
2024-01-08 18:17:10 +0100

    feat: first text

--- final.php
+++ final.php
@@ -3,3 +3,27 @@
 $testo = $_POST["testo"];
 
 ?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>PHP Badwords</title>
+  <!-- My CSS -->
+  <link rel="stylesheet" href="css/style.css">
+</head>
+<body>
+  <h1>PHP Badwords</h1>
+
+  <main>
+    <strong>Raw Text</strong>
+    <p>
+      <?php 
+        echo $testo;
+      ?>
+    </p>
+  </main>
+
+</body>
+</html>
